866695cdc4cc4e0a7214e567e991dd41ec2685ef
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-01 20:52:22 +0530

    add seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin@123'),
+            'user_type' => 'admin',
+        ]);
+    }
+}
