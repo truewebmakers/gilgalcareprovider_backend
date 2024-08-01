350e9159639d355b1662058614e379c4dabd07d2
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-01 20:44:27 +0530

    new routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/signup', [UserController::class, 'signup'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
+Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+
+
+ });
