d5b6a9b522a379dbf7832184d1b0adf2982a35ef
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-08-23 17:32:05 +0530

    add counter1

--- database/migrations/2024_08_23_105038_create_listing_views_table.php
+++ database/migrations/2024_08_23_105038_create_listing_views_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
             Schema::create('listing_views', function (Blueprint $table) {
                 $table->id();
                 $table->unsignedBigInteger('business_listing_id');
-                $table->string('ip_address');
+                $table->string('ip_address')->unique();
                 $table->timestamps();
 
+                $table->unsignedBigInteger('business_listing_id');
                 $table->foreign('business_listing_id')->references('id')->on('business_listings')->onDelete('cascade');
-                $table->unique(['business_listing_id', 'ip_address']); // Ensure IPs are unique per listing
-            });
+             });
         });
     }
 
