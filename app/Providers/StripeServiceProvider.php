3972c1ce2f1be14caf854dea94a61520cbccc2f1
TrueWebMakers <75263159+truewebmakers@users.noreply.github.com>
2024-11-13 22:58:22 +0530

    push to server6

--- app/Providers/StripeServiceProvider.php
+++ app/Providers/StripeServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Stripe\Stripe;
+use Illuminate\Support\Facades\Log;
+
 class StripeServiceProvider extends ServiceProvider
 {
     /**
@@ -14,12 +16,22 @@ class StripeServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
+    public function boot()
     {
-        //
-        Stripe::setApiKey('sk_test_51P2poYFOjqYjuziSfdIFxl7rdUrrNkIhm00XeHPVjKCeWIIGaoSPzRQKNq4pWzezAaVA8Y0LmpJGazSMYgvotkpH00OAhOSnb4');
+        try {
+            // Try to set the Stripe API key globally
+            // $apiKey = env('STRIPE_WEBHOOK_SECRET');
+            $apiKey ='sk_test_51P2poYFOjqYjuziSfdIFxl7rdUrrNkIhm00XeHPVjKCeWIIGaoSPzRQKNq4pWzezAaVA8Y0LmpJGazSMYgvotkpH00OAhOSnb4';
+            if (!$apiKey) {
+                throw new \Exception("Stripe secret key is not set in .env");
+            }
+
+            Stripe::setApiKey($apiKey);
+            Log::info("Stripe API key successfully set.");
+        } catch (\Exception $e) {
+            Log::error("Error in StripeServiceProvider boot method: " . $e->getMessage());
+        }
     }
+
+
 }
